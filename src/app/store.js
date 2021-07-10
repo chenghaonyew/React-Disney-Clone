d9e8c21bccf6d534ef6d30a738a265344317c9a1
Cheng Hao Nyew <nyewch0404@gmail.com>
2021-07-10 13:38:44 +0800

    Home Page

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import userReducer from "../features/user/userSlice";
+import movieReducer from "../features/movie/movieSlice";
 
 // Setting up redux store
 export default configureStore({
   reducer: {
     user: userReducer,
+    movie: movieReducer,
   },
   middleware: getDefaultMiddleware({
     serializableCheck: false,
